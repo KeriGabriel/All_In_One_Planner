798b6db5e4ae70a6f81dde0dc920a0594566f185
Keri <79213053+KeriGabriel@users.noreply.github.com>
2022-05-15 10:17:40 -0700

    added PlannerAPI Service, It is not implemented in app yet. Also added newtonsoft.Json Package

---
+++ All_In_One_Planner/All_In_One_Planner/Services/PlannerAPIService.cs
@@ -0,0 +1,113 @@
+﻿using All_In_One_Planner.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_In_One_Planner.Services
+{
+    public class PlannerAPIService : IData<Memo>
+    {
+        public PlannerAPIService() { }
+
+        public async Task<bool> AddMemoAsync(Memo item)
+        {
+            if (item.MemoID == null)
+            {
+                string json = JsonConvert.SerializeObject(item);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpClient client = new HttpClient();
+                string url = "https://localhost:7192/api/Memos";
+                client.BaseAddress = new Uri(url);
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await Task.FromResult(true);
+                }
+            }
+            else
+            {
+                string json = JsonConvert.SerializeObject(item);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpClient client = new HttpClient();
+                string url = "https://localhost:7192/api/Memos/" + item.MemoID;
+                client.BaseAddress = new Uri(url);
+                HttpResponseMessage response = await client.PutAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await Task.FromResult(true);
+                }
+            }
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteMemoAsync(string id)
+        {
+
+            HttpClient client = new HttpClient();
+            string url = "https://localhost:7192/api/Memos/" + id;
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.DeleteAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                return await Task.FromResult(true);
+            }
+            else
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<Memo> GetMemoAsync(string id)
+        {
+            var memo = new Memo();
+            HttpClient client = new HttpClient();
+            string url = "https://localhost:7192/api/Memos/" + id;
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = response.Content.ReadAsStringAsync().Result;
+                memo = JsonConvert.DeserializeObject<Memo>(content);
+            }
+            return await Task.FromResult(memo);
+        }
+
+        public async Task<IEnumerable<Memo>> GetMemoAsync(bool forceRefresh = false)
+        {
+            var memos = new List<Memo>();
+            HttpClient client = new HttpClient();
+            string url = "https://localhost:7192/api/Memos/";
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = response.Content.ReadAsStringAsync().Result;
+                memos = JsonConvert.DeserializeObject<List<Memo>>(content);
+            }
+            return await Task.FromResult(memos);
+        }
+
+        public async Task<bool> UpdateMemoAsync(Memo item)
+        {
+
+            string json = JsonConvert.SerializeObject(item);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            string url = "https://localhost:7192/api/Memos/" + item.MemoID;
+            client.BaseAddress = new Uri(url);
+            HttpResponseMessage response = await client.PutAsync("", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await Task.FromResult(true);
+            }
+            return await Task.FromResult(true);
+        }
+    }
+}
